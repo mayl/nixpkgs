9dcb1d346d4b7097b90dc5b6aa8bd99b78a94ae7
Larry May <192939+mayl@users.noreply.github.com>
2024-04-10 20:48:58 -0400

    update ops

--- pkgs/applications/virtualization/ops/default.nix
+++ pkgs/applications/virtualization/ops/default.nix
@@ -1,30 +1,59 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, ripgrep
+, buf
+, grpc-gateway
+, protoc-gen-go
+, protoc-gen-go-grpc
+, makeWrapper
+, sudo
+, runtimeShell
+, iproute2
+, bridge-utils
+, dnsmasq
 }:
 buildGoModule rec {
-  pname = "ops";
-  version = "0.1.32";
+  pname = "ops"; version = "0.1.41";
+
+  nativeBuildInputs = [ ripgrep buf grpc-gateway protoc-gen-go protoc-gen-go-grpc makeWrapper ];
 
   src = fetchFromGitHub {
     owner = "nanovms";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ac+17hywzyK7ChCP/nhwTP1WEIZ89+BKX9/YmsPpfg8=";
+    sha256 = "sha256-K4OcS/1/nUdMN+NUxQ6RM+dKLqQbQB3LIVmDIupeUyo=";
   };
+
+  postUnpack = ''
+    for f in $(rg --files-with-matches /bin/)
+    do
+      substituteInPlace $f --replace "/bin/" ""
+    done
+  '';
 
   proxyVendor = true; # Doesn't build otherwise
 
-  vendorHash = "sha256-65VvUy4vGTfZgsXGJVSc/yU5R5MhSKJyMMsvPOCThks=";
+  vendorHash = "sha256-xrlvhPKN9PSlgltmNPPoGZ5/OeJUZc2gsv5QbYXsu4w=";
 
   # Some tests fail
   doCheck = false;
   doInstallCheck = true;
 
   ldflags = [
-    "-s" "-w"
+    "-s"
+    "-w"
     "-X github.com/nanovms/ops/lepton.Version=${version}"
   ];
+
+  preBuild = ''
+    export HOME=$(mktemp -d)
+    make generate
+  '';
+
+  postInstall = ''
+    wrapProgram $out/bin/ops --prefix PATH ${lib.makeBinPath [iproute2 sudo bridge-utils dnsmasq runtimeShell]}
+  '';
 
   meta = with lib; {
     description = "Build and run nanos unikernels";
